ad12577ba3b52b66ca9eae7e2dbda3e2fe01efff
oniids <jdino.abaya@gmail.com>
2026-02-07 20:08:55 +0800

    add dotenv for database connection variables

--- src/plugins/db.js
+++ src/plugins/db.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 async function dbPlugin(fastify) {
   const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "Asakapa00",
-    database: "mata_tech",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
   fastify.decorate("db", pool);
